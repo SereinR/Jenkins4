23556f82778146946357910ec366c38155071681
SereinR <98984783+SereinR@users.noreply.github.com>
2026-02-12 10:30:20 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
-        double x=1/0;
-        double x=1/1;
+       double x=1/1;
     }
 }
